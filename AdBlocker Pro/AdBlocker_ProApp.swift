230860e4e8effb7d4ae7fe3bc3fb24650f59d285
NikitaKarnygin <nikita.karnygin2505@gmail.com>
2026-05-11 22:24:43 +0300

    Initial Commit

---
+++ AdBlocker Pro/AdBlocker_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdBlocker_ProApp.swift
+//  AdBlocker Pro
+//
+//  Created by Nikita on 11/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct AdBlocker_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
